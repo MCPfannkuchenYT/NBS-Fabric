a7aa267fca4ca84c2e862e6304efa9704549560f
Pfannekuchen <gamesupport@mgnet.work>
2021-05-15 14:55:56 +0200

    Create Mod

---
+++ src/main/java/de/pfannekuchen/nbs/NoteBlockPlayer.java
@@ -0,0 +1,12 @@
+package de.pfannekuchen.nbs;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NoteBlockPlayer implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+		
+	}
+
+}
